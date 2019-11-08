fe393613b050f8b26b6682d7734dd46595b23080
Iida <iida.lintuaho@gmail.com>
2019-11-08 19:39:47 +0200

    first commit

---
+++ Assets/FlashlightController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+    Quaternion newRot = new Quaternion(0,0,0,0);
+    float rot;
+    public void UpdateFlashlightDirection(Vector2 direction){
+
+        rot = Vector3.Angle(Vector3.up, direction);
+        if(direction.x >0){
+            rot = rot*-1;
+        }
+        newRot.eulerAngles = new Vector3(0,0,rot);
+        transform.rotation = newRot;
+    }
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public FlashlightController flashlight;
+    public float moveForce = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (CrossPlatformInputManager.GetAxis("Horizontal") == 0 && CrossPlatformInputManager.GetAxis("Vertical") == 0 && rb.velocity != Vector2.zero)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else if(CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetAxis("Vertical") != 0)
+        {
+            Vector2 moveVector = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+            //rb.AddForce(moveVector);
+            rb.velocity = moveVector;
+            flashlight.UpdateFlashlightDirection(moveVector);
+        }
+    }
+}
