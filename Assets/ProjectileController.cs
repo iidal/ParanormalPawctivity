c4dc06b1a41eae8c47520d4ffaf1ad82af81d4eb
Iida <iida.lintuaho@gmail.com>
2019-11-28 02:53:01 +0200

    starting to work on dialog and battle scene

---
+++ Assets/ProjectileController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 up = new Vector2(0, 0.2f);
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        rb.AddForce(up, ForceMode2D.Impulse);
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Wall"){
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BattleScene/InvaderGame/InvaderPlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvaderPlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+
+    //Moving
+    Vector3 point; // touch point
+    float targetX; // where to move player
+    Vector3 pointOnWorld;
+
+    [SerializeField]
+    Transform barrierRight, barrierLeft; // barrier game obejcts on the sides
+    
+    //Shooting
+    public GameObject projectile;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Shoot();
+        }
+
+        // if (Input.touchCount > 0)
+        // {
+
+        //     Touch touch = Input.GetTouch(0);
+        //     pointOnWorld = cam.ScreenToWorldPoint(touch.position);
+        //     targetX = pointOnWorld.x;
+
+        // if (targetX < barrierLeft.position.x)
+        // {
+        //     targetX = barrierLeft.position.x + 1;
+        // }
+        // else if (targetX > barrierRight.position.x)
+        // {
+        //     targetX = barrierRight.position.x - 1;
+        // }
+
+        //     transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetX, 1* Time.deltaTime),transform.position.y, transform.position.z);
+
+
+
+
+
+        // }
+        if (Input.GetMouseButton(0))
+        {
+            pointOnWorld = cam.ScreenToWorldPoint(Input.mousePosition);
+            targetX = pointOnWorld.x;
+            if (targetX < barrierLeft.position.x)
+            {
+                targetX = barrierLeft.position.x + 1;
+            }
+            else if (targetX > barrierRight.position.x)
+            {
+                targetX = barrierRight.position.x - 1;
+            }
+
+            transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetX, 1 * Time.deltaTime), transform.position.y, transform.position.z);
+
+        }
+    }
+    public void Shoot()
+    {
+        Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+
+
+}
